b88b658d81e9c03e9aaa857c438ac68f40d7c619
Claribel M. <claribel@1ks.ink>
2025-12-21 22:47:24 -0300

    Now with a functional blog.

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,11 +1,20 @@
+import { DateTime } from "luxon";
+
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addDateParsing(function (dateValue) {
+    if (typeof dateValue === "string") {
+      return DateTime.fromISO(dateValue, {
+        zone: "America/Argentina/Buenos_Aires",
+      });
+    }
+  });
 }
 
 export const config = {
   dir: {
     input: "views",
-    data: "content",
   },
 };
---
+++ views/blog/hi.md
@@ -0,0 +1,9 @@
+---
+layout: article
+tags: post
+title: "Hello World"
+date: 2025-12-21 23:30
+---
+
+# Hello World
+This is the start of my blog. It's finally working!
