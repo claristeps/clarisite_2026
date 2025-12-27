668f0fabffd8c497b9e9be834cf058ee3b03ef64
Claribel M. <claribel@1ks.ink>
2025-12-26 21:02:02 -0300

    Now with RSS feeds!

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,15 +1,37 @@
 import { DateTime } from "luxon";
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+
+const TIME_ZONE = "UTC-03";
 
 export default async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("styles");
 
-  eleventyConfig.addDateParsing(function (dateValue) {
-    if (typeof dateValue === "string") {
-      return DateTime.fromISO(dateValue, {
-        zone: "America/Argentina/Buenos_Aires",
-      });
-    }
+  eleventyConfig.addFilter("fmtDate", (dateObj, fmt = "yyyy-LL-dd HH:mm") => {
+    if (!dateObj) return "";
+    // Convert to desired zone
+    return DateTime.fromJSDate(dateObj, { zone: "utc" })
+      .setZone("America/Argentina/Buenos_Aires")
+      .toFormat(fmt);
+  });
+
+  eleventyConfig.addPlugin(feedPlugin, {
+    type: "rss",
+    outputPath: "/feed.xml",
+    collection: {
+      name: "post",
+      limit: 0,
+    },
+    metadata: {
+      language: "en",
+      title: "Clarifeed",
+      subtitle: "The feed for all things Clari.",
+      base: "https://claribel.ink/",
+      author: {
+        name: "Clara F. Millgress",
+        email: "",
+      },
+    },
   });
 }
 
--- views/blog/hi.md
+++ views/blog/hi.md
@@ -2,7 +2,7 @@
 layout: article
 tags: post
 title: "Hello World"
-date: 2025-12-21 23:30
+date: 2025-12-21 23:30:00
 description: "A new start for my blog!"
 ---
 
